0e21120acf20816440670d3b4f5aa6f20a04323d
Eric Gourgoulhon <eric.gourgoulhon@obspm.fr>
2013-12-30 11:50:27 +0100

    First version of SageManifolds within main Sage new git settings

--- src/sage/all.py
+++ src/sage/all.py
@@ -121,6 +121,7 @@ from sage.lfunctions.all import *
 from sage.geometry.all   import *
 from sage.geometry.triangulation.all   import *
 from sage.geometry.riemannian_manifolds.all   import *
+from sage.geometry.manifolds.all   import *
 
 from sage.dynamics.all   import *
 
