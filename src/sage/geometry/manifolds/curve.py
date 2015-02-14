9b418973cc2fc66a7765b4834f383c7f3c9855a8
Eric Gourgoulhon <eric.gourgoulhon@obspm.fr>
2015-02-14 19:47:58 +0100

    Add methods to ManifoldCurve

--- src/sage/geometry/manifolds/curve.py
+++ src/sage/geometry/manifolds/curve.py
@@ -36,8 +36,11 @@ REFERENCES:
 
 from sage.structure.sage_object import SageObject
 from sage.rings.infinity import Infinity
+from sage.symbolic.expression import Expression
+from sage.misc.latex import latex
 from sage.geometry.manifolds.domain import ManifoldOpenSubset
-from sage.geometry.manifolds.chart import MultiFunctionChart
+from sage.geometry.manifolds.manifold import RealLine
+from sage.geometry.manifolds.utilities import simplify_chain
 
 class ManifoldCurve(SageObject):
     r"""
@@ -45,9 +48,21 @@ class ManifoldCurve(SageObject):
     
     """
     def __init__(self, codomain, coord_expression, tmin=-Infinity,
-                 tmax=+Infinity, name=None, latex_name=None):
+                 tmax=+Infinity, param=None, name=None, latex_name=None):
         r"""
         Construct a curve.
+
+        TESTS::
+
+            sage: M = Manifold(2, 'M')
+            sage: X.<x,y> = M.chart()
+            sage: R.<t> = RealLine()
+            sage: from sage.geometry.manifolds.curve import ManifoldCurve
+            sage: c = ManifoldCurve(M, {X: (cos(t), sin(2*t))}, tmin=0,
+            ....:                   tmax=2*pi, name='c') ; c
+            Curve 'c' in the 2-dimensional manifold 'M'
+            sage: TestSuite(c).run()
+
         """
         if not isinstance(codomain, ManifoldOpenSubset):
             raise TypeError(
@@ -57,6 +72,13 @@ class ManifoldCurve(SageObject):
         self._codomain = codomain
         self._tmin = tmin
         self._tmax = tmax
+        if param is None:
+            self._param = RealLine().canonical_coordinate()
+        else:
+            if not isinstance(param, Expression):
+                raise TypeError("{} is not a symbolic expression".format(
+                                                                        param))
+            self._param = param
         self._name = name
         if latex_name is None:
             self._latex_name = name
@@ -72,12 +94,25 @@ class ManifoldCurve(SageObject):
             if len(expr) != n:
                 raise ValueError("{} coordinate functions must be ".format(n) + 
                                                                     "provided")
-            self._coord_expression[chart] = MultiFunctionChart(chart, *expr)
+            self._coord_expression[chart] = expr
 
 
     def _repr_(self):
         r"""
         Return a string representation of ``self``.
+
+        TESTS::
+
+            sage: M = Manifold(2, 'M')
+            sage: X.<x,y> = M.chart()
+            sage: R.<t> = RealLine()
+            sage: c = M.curve([cos(t), sin(2*t)])
+            sage: c._repr_()
+            "Curve in the 2-dimensional manifold 'M'"
+            sage: c = M.curve([cos(t), sin(2*t)], name='c')
+            sage: c._repr_()
+            "Curve 'c' in the 2-dimensional manifold 'M'"
+
         """
         description = "Curve "
         if self._name is not None:
@@ -88,6 +123,22 @@ class ManifoldCurve(SageObject):
     def _latex_(self):
         r"""
         Return a LaTeX representation of ``self``.
+
+        TESTS::
+
+            sage: M = Manifold(2, 'M')
+            sage: X.<x,y> = M.chart()
+            sage: R.<t> = RealLine()
+            sage: c = M.curve([cos(t), sin(2*t)])
+            sage: c._latex_()
+            "\\mbox{Curve in the 2-dimensional manifold 'M'}"
+            sage: c = M.curve([cos(t), sin(2*t)], name='c')
+            sage: c._latex_()
+            'c'
+            sage: c = M.curve([cos(t), sin(2*t)], name='c', latex_name=r'\gamma')
+            sage: c._latex_()
+            '\\gamma'
+
         """
         if self._latex_name is None:
             return r'\mbox{' + str(self) + r'}'
@@ -107,6 +158,23 @@ class ManifoldCurve(SageObject):
 
         - True if ``self`` is equal to ``other``,  or False otherwise
 
+        TESTS::
+
+            sage: M = Manifold(2, 'M')
+            sage: X.<x,y> = M.chart()
+            sage: R.<t> = RealLine()
+            sage: c = M.curve([cos(t), sin(2*t)], name='c')
+            sage: c.__eq__(3)
+            False
+            sage: c.__eq__(M.curve([cos(t), sin(2*t)]))
+            True
+            sage: c.__eq__(M.curve([cos(t), sin(2*t)], name='d'))
+            True
+            sage: c.__eq__(M.curve([cos(t), sin(2*t)], tmin=0))
+            False
+            sage: c.__eq__(M.curve([cos(3*t), sin(2*t)]))
+            False
+
         """
         if not isinstance(other, ManifoldCurve):
             return False
@@ -116,12 +184,190 @@ class ManifoldCurve(SageObject):
             return False
         if self._tmax != other._tmax:
             return False
-        for chart, functions in self._coord_expression.iteritems():
+        for chart, expr in self._coord_expression.iteritems():
             try:
-                if functions.expr() != other.expr(chart):
+                if expr != other.expr(chart):
                     return False
             except ValueError:
                 return False
         return True
 
 
+    def __ne__(self, other):
+        r"""
+        Inequality operator.
+
+        INPUT:
+
+        - ``other`` -- another instance of :class:`ManifoldCurve` to compare
+          with
+
+        OUTPUT:
+
+        - True if ``self`` is different from ``other``,  or False otherwise
+
+        TESTS::
+
+            sage: M = Manifold(2, 'M')
+            sage: X.<x,y> = M.chart()
+            sage: R.<t> = RealLine()
+            sage: c = M.curve([cos(t), sin(2*t)], name='c')
+            sage: c.__ne__(M.curve([cos(3*t), sin(2*t)]))
+            True
+
+        """
+        return not self.__eq__(other)
+
+    def __cmp__(self, other):
+        r"""
+        Old-style (Python 2) comparison operator.
+
+        This is provisory, until migration to Python 3 is achieved.
+
+        TESTS::
+
+            sage: M = Manifold(2, 'M')
+            sage: X.<x,y> = M.chart()
+            sage: R.<t> = RealLine()
+            sage: c = M.curve([cos(t), sin(2*t)], name='c')
+            sage: c.__cmp__(M.curve([cos(t), sin(2*t)]))
+            0
+            sage: c.__cmp__(M.curve([cos(3*t), sin(2*t)]))
+            -1
+
+        """
+        if self.__eq__(other):
+            return 0
+        else:
+            return -1
+
+    def expr(self, chart=None):
+        r"""
+        Return the expression of ``self`` in a given chart.
+
+        INPUT:
+
+        - ``chart`` -- (default: None) chart on the curve's codomain; if
+          ``None``, the codomain's default chart is assumed
+
+        OUTPUT:
+
+        - symbolic expression representing the curve in the above chart
+
+        EXAMPLES:
+
+        EXAMPLES:
+
+        Cartesian and polar expression of a curve in the Euclidean plane::
+
+            sage: Manifold._clear_cache_() # for doctests only
+            sage: M = Manifold(2, 'R^2', r'\RR^2')  # the Euclidean plane R^2
+            sage: c_xy.<x,y> = M.chart() # Cartesian coordinate on R^2
+            sage: U = M.open_subset('U', coord_def={c_xy: (y!=0, x<0)}) # the complement of the segment y=0 and x>0
+            sage: c_cart = c_xy.restrict(U) # Cartesian coordinates on U
+            sage: c_spher.<r,ph> = U.chart(r'r:(0,+oo) ph:(0,2*pi):\phi') # spherical coordinates on U
+            sage: # Links between spherical coordinates and Cartesian ones:
+            sage: ch_cart_spher = c_cart.coord_change(c_spher, sqrt(x*x+y*y), atan2(y,x))
+            sage: ch_cart_spher.set_inverse(r*cos(ph), r*sin(ph))
+            Check of the inverse coordinate transformation:
+               x == x
+               y == y
+               r == r
+               ph == arctan2(r*sin(ph), r*cos(ph))
+            sage: R.<t> = RealLine()
+            sage: c = U.curve({c_spher: (1,t)}, tmin=0, tmax=2*pi, name='c')
+            sage: c.expr(c_spher)
+            (1, t)
+            sage: c.expr(c_cart)
+            (cos(t), sin(t))
+
+        Since ``c_cart`` is the default chart on ``U``, it can be omitted::
+
+            sage: c.expr()
+            (cos(t), sin(t))
+
+        Cartesian expression of a cardiod::
+        
+            sage: c = U.curve({c_spher: (2*(1+cos(t)), t)}, tmin=0, tmax=2*pi, name='c')
+            sage: c.expr(c_cart)
+            (2*cos(t)^2 + 2*cos(t), 2*(cos(t) + 1)*sin(t))
+
+        """
+        codom = self._codomain
+        def_chart = codom._def_chart
+        if chart is None:
+            chart = def_chart
+        if chart not in self._coord_expression:
+            # Some computation must be performed
+            sel_chart = None # selected chart
+            if def_chart in self._coord_expression \
+                                and (def_chart, chart) in codom._coord_changes:
+                sel_chart = def_chart
+            else:
+                for ochart in self._coord_expression:
+                    if (ochart, chart) in codom._coord_changes:
+                        sel_chart = ochart
+                        break
+            if sel_chart is None:
+                raise ValueError("No start chart has been found to compute " +
+                      "the expression of {} in the {}".format(self, chart))
+            oexpr = self._coord_expression[sel_chart]
+            chg = codom._coord_changes[(sel_chart, chart)]
+            self._coord_expression[chart] = chg(*oexpr)
+        return self._coord_expression[chart]
+
+    def __call__(self, t, simplify=True):
+        r"""
+        Image of a given value of the parameter.
+
+        EXAMPLES:
+
+        Points on circle in the Euclidean plane::
+        
+            sage: M = Manifold(2, 'M')
+            sage: X.<x,y> = M.chart()
+            sage: R.<t> = RealLine()
+            sage: c = M.curve([cos(t), sin(t)], name='c')
+            sage: c(0)
+            point 'c(0)' on 2-dimensional manifold 'M'
+            sage: c(0) in M
+            True
+            sage: c(0).coord(X)
+            (1, 0)
+            sage: c(pi/4).coord(X)
+            (1/2*sqrt(2), 1/2*sqrt(2))
+            sage: c(t)
+            point 'c(t)' on 2-dimensional manifold 'M'
+            sage: c(t).coord(X)
+            (cos(t), sin(t))
+        
+        """
+        codom = self._codomain
+        def_chart = codom._def_chart
+        if def_chart in self._coord_expression:
+            chart = def_chart
+        else:
+            chart = self._coord_expression.keys()[0]  # a chart is picked at
+                                                      # random
+        expr = self._coord_expression[chart]
+        n = len(expr)
+        dict_subs = {self._param: t}
+        coords = [expr[i].substitute(dict_subs) for i in range(n)]
+        if simplify:
+            coords = [simplify_chain(coords[i]) for i in range(n)]
+        if self._name is not None:
+            name = self._name + "({})".format(t)
+        else:
+            name = None
+        if self._latex_name is not None:
+            latex_name = self._latex_name + r"\left(" + latex(t) + r"\right)"
+        else:
+            latex_name = None
+        return codom.point(coords=coords, chart=chart, name=name,
+                           latex_name=latex_name)
+
+
+
+
+
+        
